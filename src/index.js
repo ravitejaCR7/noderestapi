7045f5525c7309de6c347168c65f2df35aff4215
ravitejacr7 <raviteja7.talari@gmail.com>
2019-04-16 13:51:45 -0500

    Initial push of node project

---
+++ src/index.js
@@ -0,0 +1,56 @@
+
+const mongoose = require('mongoose');
+const express = require('express');
+var bodyParser = require('body-parser');
+console.log("Roonaldoooooo!");
+
+
+let app = express();
+
+app.use('/uploads',express.static('uploads'));
+app.use('/PostsBinaryData',express.static('PostsBinaryData'));
+app.use(bodyParser.json( {limit: '50mb', extended: true} ));
+app.use(bodyParser.urlencoded({limit: '50mb' ,extended: true}));
+
+mongoose.connect('mongodb://localhost/testaroo',{useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+
+mongoose.Promise = global.Promise;
+
+mongoose.connection.once('open', function () {
+    console.log('connection has been made');
+}).on('error', function () {
+    console.log('failed to connect to mongoose');
+});
+
+
+
+let personsRoute = require('./routes/person');
+
+app.use( (req, res, next) => {
+    console.log(`${new Date().toString()} ====> ${req.originalUrl} `);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if(req.method === "OPTIONS")
+    {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.use('/person', personsRoute);
+app.use(express.static('public'));
+
+// error handling
+app.use((req, res, next) => {
+    res.status(404).send("We think you are lost!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> console.info(`server started listening on ${PORT}`));
---
+++ src/routes/person.js
@@ -0,0 +1,136 @@
+let express = require('express');
+let router = express.Router();
+
+const multer = require('multer');
+const storageStrategy = multer.diskStorage(
+    {
+        destination: function (req, file, cb) {
+            cb(null, './uploads/');
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname);
+        }
+    }
+);
+
+const storageStrategyForPosts = multer.diskStorage(
+    {
+        destination: function (req, file, cb) {
+            cb(null, './PostsBinaryData/');
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname);
+        }
+    }
+);
+
+const fileFilters = function (req, file, cb) {
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png')
+    {
+        cb(null, true);
+    }
+    else
+    {
+        cb(new Error('Wrong file type - profile pic'), false);
+    }
+};
+
+const fileFiltersForPosts = function (req, file, cb) {
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'video/mp4')
+    {
+        cb(null, true);
+    }
+    else
+    {
+        cb(new Error('Wrong file type - post data'), false);
+    }
+};
+
+const upload = multer(
+    {
+        storage: storageStrategy,
+        limits: {
+            fileSize: 1024 * 1024 * 10
+        },
+        fileFilters
+    }
+);
+
+const uploadVideos = multer(
+    {
+        storage: storageStrategy,
+        limits: {
+            fileSize: 1024 * 1024 * 10
+        }
+    }
+);
+
+const uploadImagesAndVideosToPosts = multer(
+    {
+        storage: storageStrategyForPosts,
+        limits: {
+            fileSize: 1024 * 1024 * 10
+        },
+        fileFiltersForPosts
+    }
+);
+
+// Require the controllers WHICH WE DID NOT CREATE YET!!
+var userTable_controller = require('../controllers/usertable.controller');
+
+
+// a simple test url to check that all of our files are communicating correctly.
+router.get('/test', userTable_controller.test);
+
+router.post('/newUserCreation', upload.single('personPic'),userTable_controller.user_create);
+router.post('/newUserCreationVideo', uploadVideos.single('personVideo'),userTable_controller.user_create_video);
+
+router.get('/userInfo/:id', userTable_controller.user_details);
+
+router.delete('/deleteUser/:id/delete', userTable_controller.user_delete);
+
+router.put('/:id/update', userTable_controller.user_update);
+
+router.get('/specificUserInfo/:email', userTable_controller.specific_user_details);
+
+//login API
+
+//get email and password from front end
+router.get('/loginCheck/:email/:password', userTable_controller.specific_user_Login_check);
+
+
+//Search for users
+router.get('/searchName/:name', userTable_controller.search_user_details);
+
+//queryStrings
+router.get('/find', (req, res) => {
+    if(req.query.name)
+    {
+        res.status(200).send(`person method -- queryStr ${req.query.name} `);
+    }
+    else
+    {
+        res.send("person method");
+    }
+});
+
+//paramProperties
+router.get('/sampleGet2/:name', (req, res) => {
+    res.send(`person method -- param property ${req.params.name}`);
+});
+
+
+
+//privacyTableCalls
+router.get('/privacySettings/:email', userTable_controller.user_privacy_get);
+
+router.post('/privacySettingsCreate', userTable_controller.user_privacy_post);
+
+router.put('/privacySettingsChange/:email', userTable_controller.user_privacy_update);
+
+
+//PostsMadeByUser
+router.post('/postedByThisUser',uploadImagesAndVideosToPosts.single('personPostBinary') , userTable_controller.user_posting_post);
+
+
+module.exports = router;
